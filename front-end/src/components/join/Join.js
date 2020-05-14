0fa1a4d19f3b0194cc8234dca8a7c161b73061dd
Peter Solomon <petersolomon704@gmail.com>
2020-05-14 23:15:09 +0100

    CSS

--- front-end/src/components/join/Join.js
+++ front-end/src/components/join/Join.js
@@ -11,10 +11,10 @@ const Join = () => {
     return (
         <div className='form-wrapper'>
             <form className ='join-form'>
-                <div className='click-join'><h4 className='join-text'>Click to Join</h4></div>
+                <div className='click-join'><h4 className='join-text'>Welcome to Purple Chat</h4></div>
                 <input type="text" className='jointInputOne' placeholder='Name' onChange={(e) => setName(e.target.value)} />
                 <br/>
-                <input type="text" className='joinInputTwo' placeholder='Room' onChange={(e) => setRoom(e.target.value)} />
+                <input type="text" className='joinInputTwo' placeholder='Course' onChange={(e) => setRoom(e.target.value)} />
                <br/>
                 <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                     <button className='sign-in' type='submit'>Sign In</button>
